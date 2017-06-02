938040cfaca5b5fedb159660f60e4e5ee9d48a23
761917 <761917@apps.district196.org>
2017-06-01 19:48:28 -0500

    Add files via upload

--- euro/euro.js
+++ euro/euro.js
@@ -2,50 +2,118 @@ var icons, numIcons;
 var table;
 var questions, cur;
 var player;
+var end;
 
 function preload()
 {
-  table=loadTable("data/euro.csv","csv","header"); 
+  numIcons=4;
+  icons=[];
+  for (var i=1; i<=numIcons; i++)
+    icons.push(loadImage("data/"+i+".png"));
+  table=loadTable("data/euro.csv", "csv", "header");
 }
 function setup() { 
   questions=[];
-  createCanvas(windowWidth,windowHeight);
-  for(var r=0; r<table.getRowCount(); r++)
+  createCanvas(windowWidth, windowHeight);
+  for (var r=0; r<table.getRowCount(); r++)
   {
     questions.push(new Question(r));
   }
+  for (var i=0; i<numIcons; i++)
+  {
+    icons[i].resize(width/10, width/10);
+  }
   cur=0;
-  player={money:10,people:10,clergy:10,army:10};
-  textAlign(CENTER);
+  player={money:15, people:15, clergy:15, army:15};
+  textAlign(CENTER, CENTER);
+  imageMode(CENTER);
   textSize(width/30);
+  end=false;
 }
 
 function draw() {
-  background(0);
-  fill(0,255,0);
-  rect(0,2*height/3,width/2,height/3);
-  fill(255,0,0);
-  rect(width/2,2*height/3,width/2,height/3);
-  fill(255);
-  text(player.money+","+player.people+","+player.clergy+","+player.army,width/2,height/10);
-  text(questions[cur].text,width/4,height/5,width/2,2*height/3);
-  text("YES",width/4,5*height/6);
-  text("NO",3*width/4,5*height/6);
+  if (player.money>0 && player.people>0 && player.clergy>0 && player.army>0 && end==false)
+  {
+    background(0, 0, 255);
+    if (mouseIsPressed && mouseX<width/2 && mouseY>2*height/3)
+      fill(100, 150, 100);
+    else
+      fill(0, 255, 0);
+    rect(0, 2*height/3, width/2, height/3);
+    if (mouseIsPressed && mouseX>width/2 && mouseY>2*height/3)
+      fill(150, 100, 100);
+    else
+      fill(255, 0, 0);
+    rect(width/2, 2*height/3, width/2, height/3);
+    fill(255);
+    for (var i=0; i<numIcons; i++)
+    {
+      image(icons[i], (i+1)*width/5, height/10);
+    }
+    text(player.money, (width/5)+(width/20), height/10);
+    text(player.people, (2*width/5)+(width/20), height/10);
+    text(player.clergy, (3*width/5)+(width/20), height/10);
+    text(player.army, (4*width/5)+(width/20), height/10);
+    text(questions[cur].text, width/4, height/5, width/2, height/2);
+    text("YES", width/4, 5*height/6);
+    text("NO", 3*width/4, 5*height/6);
+  }
+  else if(end==true)
+  {
+    background(255);
+    fill(0);
+    text("You successfully brought your country out of the Middle Ages and into the 21st century.",width/4,0,width/2,height/2);
+    text("Final Stats:\nGDP: "+player.money+"\nFavorability with the People: "+player.people+"\nFavorability with the Church: "+player.clergy+"\nMilitary Might: "+player.army,width/4,height/3,width/2,height/2);
+  }
+  else if(player.army<=0)
+  {
+    background(0);
+    fill(255);
+    text("Your army was too weak and you were invaded",width/4,height/4,width/2,height/2);
+  }
+  else if(player.money<=0)
+  {
+    background(0);
+    fill(255);
+    text("You ran out of money and your country went bankrupt",width/4,height/4,width/2,height/2);
+  }
+  else if(player.people<=0)
+  {
+    background(0);
+    fill(255);
+    text("Unable to rule by love or by fear, you have been beheaded by your own angry people",width/4,height/4,width/2,height/2);
+  }
+  else
+  {
+    background(0);
+    fill(255);
+    text("You managed to upset the church so much that God himself struck you with a bolt of lightning",width/4,height/4,width/2,height/2);
+  }
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   textSize(width/30);
-}
-function mousePressed()
-{
-  if(mouseX<width/2 && mouseY>2*height/3 && cur+1<table.getRowCount())
+  for (var i=0; i<numIcons; i++)
   {
-    questions[cur].yes(true);
-    cur++;
+    icons[i].resize(width/10, width/10);
   }
-  if(mouseX>width/2 && mouseY>2*height/3 && cur+1<table.getRowCount())
+}
+function mouseClicked()
+{
+  if (mouseX<width/2 && mouseY>2*height/3)
   {
     questions[cur].yes(true);
-    cur++;
+    if(cur+1<table.getRowCount())
+      cur++;
+    else
+      end=true;
+  }
+  if (mouseX>width/2 && mouseY>2*height/3 && cur+1<table.getRowCount())
+  {
+    questions[cur].yes(false);
+    if(cur+1<table.getRowCount())
+      cur++;
+    else
+      end=true
   }
 }
